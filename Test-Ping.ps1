34f4c6d63edb3ec627e141c91a32f27761fad582
tobiaspsp <tobias@powershell.one>
2023-07-19 14:40:10 +0200

    Create Test-Ping.ps1

---
+++ Test-Ping.ps1
@@ -0,0 +1,57 @@
+﻿function Test-Ping
+{
+    <#
+            .SYNOPSIS
+            Short Description
+            .DESCRIPTION
+            Detailed Description
+            .EXAMPLE
+            Test-Ping
+            explains how to use the command
+            can be multiple lines
+            .EXAMPLE
+            Test-Ping
+            another example
+            can have as many examples as you like
+    #>
+    param
+    (
+        [Parameter(Mandatory,ValueFromPipeline)]
+        [string]
+        $ComputerName,
+        
+        [int]
+        $TimeoutMillisec = 1000
+    )
+    
+    
+    
+    begin
+    {
+        $obj = [System.Net.NetworkInformation.Ping]::new()
+    }
+    
+    process
+    {
+        try
+        {
+            $obj.Send($ComputerName, $TimeoutMillisec) |
+            Select-Object -Property @{N='HostName';E={$ComputerName}},@{N='IP';E={$_.Address}}, @{N='Port';E={'ICMP'}}, @{N='Response';E={$_.Status -eq 'Success'}}
+        }
+        # NOTE: When you use a SPECIFIC catch block, exceptions thrown by -ErrorAction Stop MAY LACK
+        # some InvocationInfo details such as ScriptLineNumber.
+        # REMEDY: If that affects you, remove the SPECIFIC exception type [System.Net.NetworkInformation.PingException] in the code below
+        # and use ONE generic catch block instead. Such a catch block then handles ALL error types, so you would need to
+        # add the logic to handle different error types differently by yourself.
+        catch [System.Net.NetworkInformation.PingException]
+        {
+            Write-Warning "$Computername ist kein gültiger Computername"
+        }
+    }
+
+    end
+    {    
+        $obj.Dispose()
+    }
+}
+
