035954b28def6292c726c0091fc1acc910cec47c
tobiaspsp <tobias@powershell.one>
2023-07-21 12:54:04 +0200

    neue funktionen

---
+++ Get-SpecialFolderPath.ps1
@@ -0,0 +1,23 @@
+﻿function Get-SpecialFolderPath
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+        [Environment+SpecialFolder]
+        $FolderId
+        
+    )
+    
+    process
+    {
+        try
+        {
+            [System.Environment]::GetFolderPath($FolderId)
+        }
+        catch
+        {
+            Write-Warning "Error occured höhöhö: $_"
+        }
+    }
+}
